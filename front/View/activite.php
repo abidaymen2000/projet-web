51bf7e6f46497af52bfd92ef0672de952654429a
abidaymen2000 <74994789+abidaymen2000@users.noreply.github.com>
2020-12-24 17:01:15 +0100

    Add files via upload

---
+++ front/View/activite.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bodybuild</title>
+        <link rel="stylesheet" href="../assetes/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assetes/css/flexslider.css">
+        <link rel="stylesheet" href="../assetes/css/jquery.fancybox.css">
+        <link rel="stylesheet" href="../assetes/css/responsive.css">
+        <link rel="stylesheet" href="../assetes/css/font-icon.css">
+        <link rel="stylesheet" href="../assetes/css/animate.min.css">
+        <link rel="stylesheet" href="../assetes/css/style.css">
+        <link rel="stylesheet" href="../assetes/css/recherche.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+
+	</head>
+
+	<body>  
+   
+        <section class="banner" role="banner">
+            <header id="header">
+                <div class="main">
+            <input type="search"  id="search" name="search" placeholder="Recherche"> 
+            <div class='btn'>
+                <a href="#"><i class="fas fa-search"></i></a>
+              </div>
+              </div>
+              <div class="header-content clearfix"> <span class="logo">Body build</span>
+                <nav class="navigation" role="navigation">
+                  <ul class="primary-nav">
+                    <li><a href="#intro">Intro</a></li>
+                    <li><a href="#type">Les types de musculation</a></li>
+                    <li><a href="#methodes">Les méthodes les plus populaires</a></li>
+                    <li><a href=index.php#body>Acceuil</a></li>
+                       
+                  </ul>
+                </nav>
+                <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+            </header>
+            <div class="container">
+                <div class="col-md-10 col-md-offset-1">
+                  <div class="banner-text text-center">
+                    <h1>Quels sont les différents types de musculation possible ?</h1>
+                    </div>
+                </div>
+              </div>
+            </section>
+            <br />
+            <div class="alert alert-info">
+                <p>Pour <strong>faire travailler vos muscles</strong>, il existe une multitude de méthodes. Selon l’objectif que vous visez, <strong>différents types d’exercices</strong> seront adaptés à votre entraînement.<br/>
+                    Nous avons vu dans un précédent article comment préparer un planning de musculation efficace et pertinent, nous allons aujourd’hui nous intéresser aux différents types de travail que vous pouvez effectuer dans le cadre de la musculation.
+                <br/> </p>
+            </div>
+            
+    <section id="intro" class="section intro">
+                <div class="container">
+                    <div class="col-md-8 col-md-offset-2 text-center">
+                        <h2>"DÉCOUVRIR TOUS LES SECRETS"</h2>
+                        <h2>"PRISE DE MUSCLE RAPIDE SANS GRAS"</h2>
+                    </div>
+                </div>
+    </section>
+            <br />
+             <div class="container">
+                <div>
+               <img src="../assetes/img/body2.jpg" alt="body2" style="width:600px;height:300px;">
+                </div>
+             </div>
+
+             <!-- musculation section -->
+    <section class="musculation">
+          <div class="container"> 
+            <h3>La musculation: le travail sur le muscle</h3>
+             <p>Pratiquer la musculation implique de forcer sur ses muscles pour les faire travailler. Lorsque le muscle subit un choc, les fibres musculaires sont sollicitées. En se reconstruisant, c’est elles qui permettent de développer la masse musculaire.</p>
+             <br>
+             <p>En parallèle, voici également une <strong>vidéo explicative</strong> des résultats à espérer après des mois de musculation :</p>
+             <div class="video">
+             <iframe width="853" height="480" src="https://www.youtube.com/embed/2yZuxkuyWA8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+            </iframe>
+             </div>
+        <div>
+            <h3>Les contractions du muscle</h3>
+            <p>
+                Avant de s’intéresser aux différents types de musculation, il est primordial de comprendre le fonctionnement du muscle pendant l’effort, afin de pouvoir réaliser des gestes et des séries efficaces. On distingue différents types de contraction, qui interviennent tour à tour lors de votre entraînement.
+            </p>
+            <p>
+                La contraction concentrique : il s’agit de la plus connue. C’est celle qui intervient lorsque vous mettez votre muscle en action. Lorsque le muscle se met en mouvement, les unités contractiles se rapprochent et provoquent le gonflement.
+            </p>
+            <p>
+                La contraction excentrique : c’est l’effet contraire. Il s’agit de l’étirement du muscle, qui survient lorsque vous relâchez. Cette contraction permet de contrôler et de freiner le mouvement.
+            </p>
+            <p>
+                La contraction isométrique : ce type de contraction se caractérise par l’absence de mouvement. Il s’agit de la résistance du muscle à une pression ou à une charge. Lorsque le muscle est en contraction isométrique, il faut jouer sur la durée pour le faire travailler. A l’inverse, les contractions excentriques et concentriques impliquent un certain nombre de répétitions pour agir sur le muscle.
+            </p>
+        </div>
+    </div>
+    </section>
+         <!-- intro section -->
+
+    <section id="type" class="musculation">
+        <div class="container">
+            <h3>Musculation au poids de corps et musculation avec charges</h3>
+            <p>
+                Nous verrons plus en détail <strong>les exercices à pratiquer en fonction de votre objectif</strong> et <stong>les méthodes de musculation les plus réputées</stong>, dans les paragraphes suivants. Il est cependant important de faire la différence entre deux types d’entraînement de musculation :
+            </p>
+            <p>
+                <strong>La musculation au poids de corps</strong> : Cette <strong>méthode de travail</strong> ne nécessite aucun accessoire, vous n’aurez besoin que de votre corps pour la pratiquer. Elle est essentielle lorsque l’on débute : il est évidemment important de d’abord maîtriser les mouvements avant d’y ajouter les charges. Mais elle peut aussi convenir à des pratiquants de musculation d’un niveau plus avancé, car différentes astuces permettent d’ajouter de la difficulté, sans rajouter de charges externes.
+            </p>
+            <p>
+                De nombreuses idées reçues laissent à penser que la musculation au poids de corps n’est plus efficace à force de s’entraîner : c’est tout à fait faux. <strong>Ce type de musculation</strong> peut être <strong>beaucoup plus intense qu’avec des charges</strong>, selon le positionnement de votre corps ou selon les appuis au sol.
+            </p>
+            <p>
+                <strong>La musculation avec poids libres</strong> : Ce <strong>type de travail</strong> implique de réaliser les mouvements <strong>en ajoutant une charge grâce à des haltères, des disques, des kettlebells.</strong> En ajoutant du poids, vous augmentez l’intensité de l’entraînement et vous invitez votre muscle à travailler davantage. Les poids libres vous donnent l’avantage de pouvoir ajuster la charge en fonction de votre niveau et de suivre plus facilement votre évolution.
+            </p>
+            <img src="../assetes/img/body3.jpg" alt="body3" style="width:336px;height:450px;">
+            <br />
+            <p>
+                Il faut savoir que <strong>ces deux types d’entraînement</strong> peuvent tout à fait être complémentaires et qu’il n’existe pas une méthode plus efficace que l’autre.Vous pouvez tout à fait <strong>inclure des exercices au poids de corps et des séries avec des charges dans votre programme de musculation.</strong>
+            </p>
+            <p style="color:rgb(6, 139, 46);text-align:center;font-size:150%;"> 
+                Justement, casser la routine est un excellent moyen de rebooster votre motivation et d’obtenir des résultats probants.
+            </p>
+        
+            <h3>Le type de musculation selon votre objectif</h3>
+           <p style="color:rgb(6, 139, 46);text-align:center;font-size:150%;">
+            Si différentes méthodes d’entraînement existent, il est primordial de <strong>définir vos objectifs</strong> avant de vous orienter vers un type de musculation.
+           </p>
+           <h4 style="font-family:Optima;">Sèche et perte de gras</h4>
+           <p>
+            Si vous débutez la musculation avec pour objectif de perdre votre gras et de sécher, vous devrez adapter vos séances. Pour perdre du poids rapidement, il est important d’inclure autant de cardio dans votre entraînement que de musculation. L’équilibre entre les deux est primordial car c’est ce qui vous permettra de ne pas perdre de masse musculaire.
+           </p>
+           <p>
+            Un programme mixte est une alternative idéale pour avoir un impact considérable sur la perte de gras : il sera impossible d’obtenir les mêmes résultats en effectuant uniquement du cardio ou de la musculation. Dans votre programme de sèche, il faudra recourir à des charges légères à moyennes : votre régime alimentaire vous imposera des restrictions caloriques, vos capacités seront donc réduites.
+           </p>
+           <h4>Prise de masse</h4>
+           <p>
+            La prise de masse est une phase d’entraînement intense et spécifique pour prendre de la masse musculaire rapidement. Cette étape doit intervenir après une sèche car elle n’est pas pertinente si votre taux de gras est trop important. La prise de masse doit s’envisager uniquement si votre physique est adapté.
+           </p>
+           <p>
+            Ce type de musculation va impliquer d’effectuer des exercices de base avec des charges lourdes. Pour ne pas stagner, vous devrez être réactif et augmenter régulièrement vos charges. Votre muscle s’habituera rapidement à l’effort et ne progressera plus si vous ne variez pas les poids. Vous devez atteindre une intensité maximale en entraînement.
+           </p>
+        
+            <h3 style=>Le type de musculation selon votre morphologie</h3>
+            <p style="color:rgb(6, 139, 46);text-align:center;font-size:150%;">
+                Le type de musculation pourra se définir également <strong>en fonction de votre morphologie.</strong> Même si vous rêvez d’avoir des muscles de bodybuilder, il faut savoir que certains corps ne s’y prêtent pas et que votre morphologie de base sera déterminante dans votre programme d’entraînement.
+            </p>
+            <h4>Vous êtes ectomorphe</h4>
+            <p>
+                <strong>Ce type de morphologie</strong> correspond aux personnes minces. Ce sont généralement celles qui peuvent manger de tout et qui prendront rarement du poids. Dans ce cas de figure, cela signifie aussi <strong>qu’il sera plus difficile de prendre de la masse musculaire.</strong> L’ectomorphe devra privilégier un entraînement court avec des exercices de base. Pour prendre du muscle, il devra prendre du poids et donc multiplier les repas.
+            </p>
+            <h4>Vous êtes endomorphe</h4>
+            <p>
+                Les endomorphes ont des os plus lourds ainsi qu’une taille et des hanches plus larges. Ils ne rencontrent aucun problème pour prendre du poids mais éprouvent plus de difficultés pour s’en débarrasser. L’endomorphe ne devra pas seulement s’attarder sur la musculation mais aussi <strong>réaliser des entraînements de cardio réguliers.</strong> Concernant le <strong>type de musculation</strong>, il devra s’entraîner avec des charges modérées.
+            </p>
+            <h4>Vous êtes mésomorphe</h4>
+            <p>
+                Cette morphologie est idéale pour être bodybuilder. Caractérisée par un physique taillé « en V », elle permet de <strong>supporter une grande masse musculaire</strong> tout en conservant une belle harmonie. Les mésomorphes pourront miser sur un <strong>entraînement long et intense</strong> et pratiquer le cardio de manière modérée.
+            </p>
+        </div>
+    </section>
+     
+    <section id="methodes" class="musculation">
+        <div class="container">
+            <h3>Les méthodes les plus populaires</h3>
+            <p style="color:rgb(6, 139, 46);text-align:center;font-size:150%;">
+                Parmi les <strong>différents types de musculation</strong>, certaines méthodes ont fait leurs preuves et sont très répandues dans l’univers de la salle. Zoom sur ces entraînements réputés !
+            </p>
+            <h4>Full body</h4>
+            <p>
+                Le Full Body implique de <strong>travailler la totalité du corps pendant une séance.</strong> Tous les muscles sont sollicités et on retrouve lors d’un entraînement la plupart des mouvements de base. Ce <strong>type de travail</strong> peut être réalisé sur deux ou trois séances par semaine : comme tous les groupes musculaires sont mis à rude épreuve, il est important d’observer des jours de repos.
+            </p>
+            <h4>Split body</h4>
+            <p>
+                Contrairement au Full body, <strong>ce type d’entraînement se focalise sur quelques groupes musculaires lors d’une séance.</strong> Par exemple, vous allez travailler le lundi les abdominaux, les cuisses et les fessiers le lendemain et les épaules et les triceps le jour suivant. En travaillant ainsi, vos muscles auront le temps de récupérer et vous pourrez réaliser des entraînements pratiquement tous les jours (conservez tout de même un jour de repos total par semaine).
+            </p>
+            <h4>Cross Fit</h4>
+            <p>
+                Le Cross Fit fait de nombreux nouveaux adeptes chaque jour ! <strong>Cette méthode d’entraînement américaine</strong> combine <strong>des exercices de cardio à des séries de musculation.</strong> Une séance dure généralement entre 30 et 45 minutes et se révèle être très intense.
+            </p>
+            <h4>Le Hiit</h4>
+            <p>
+                « L’entraînement par intervalles à haute intensité » est un <strong>type de musculation</strong> qui propose <strong>des phases d’exercices à haute intensité, combinées à des étapes de récupération active.</strong> Très plébiscitée par les personnes qui manquent de temps et qui souhaitent des résultats rapides, cette méthode augmente vos performances grâce à des exercices très courts mais très intenses. Elle regroupe :
+            </p>
+            <h5>La méthode Tabata</h5>
+            <p>
+                C’est un travail <strong>cardio très intense et très court</strong> qui s’effectue sur des séries de 20 secondes, suivies d’une pause de 10 secondes. Le circuit doit être réalisé 8 fois.
+            </p>
+            <h5>La méthode Little</h5>
+            <p>
+                Sur le même principe, <strong>ce type d’entraînement</strong> consiste à travailler pendant 60 secondes puis à observer une phase de récupération de 75 secondes.
+            </p>
+            <h4>Méthode Lafay</h4>
+            <p>
+                Cette <strong>méthode</strong> est <strong>très répandue et appréciée</strong> car elle permet de réaliser des exercices sans aucun matériel, uniquement au poids de corps. Très pratique, elle inclue des mouvements basiques et simples à mémoriser, à effectuer n’importe où.
+            </p>
+            <h4>Le TRX</h4>
+            <p>
+                Le « Total Résistance Exercice » s’effectue au poids de corps, grâce à des sangles ajustables. De plus en plus présent dans les salles de sport, le TRX est un moyen efficace de s’entraîner sans charge extérieure et de varier les exercices, pour casser la routine.
+            </p>
+        </div>
+    </section>
+
+    <br />
+    <br />
+
+ <!-- musculation section -->
+
+ <!-- Footer section -->
+ <footer class="footer">
+    <div class="container-fluid">
+    <div id="map-row" class="row">
+        <div class="col-xs-12">    
+            <iframe width="100%" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&source=s_q&hl=en&geocode=&q=15+Springfield+Way,+Hythe,+CT21+5SH&aq=t&sll=52.8382,-2.327815&sspn=8.047465,13.666992&ie=UTF8&hq=&hnear=15+Springfield+Way,+Hythe+CT21+5SH,+United+Kingdom&t=m&z=14&ll=51.077429,1.121722&output=embed"></iframe> 
+          
+              <div id="map-overlay" class="col-xs-5 col-xs-offset-6">
+                <h2 style="margin-top:0;color:#fff;">Contact us</h2>
+                <address style="color:#fff;">
+                    <strong>Company name</strong><br>
+                    Life style.<br>
+                    Marsa-tn<br>
+                    Tunis<br>
+                    V6G 1G9<br>
+                    <abbr title="Phone">Tel:</abbr> (216) 21-461-637
+                </address>
+                  © 2020 Company Name. <strong>Life style.</strong>
+            </div>
+        </div>
+         </div>
+    </div>
+    </footer>
+    <!-- Footer section -->
+
+    </body>
+
+</html>
